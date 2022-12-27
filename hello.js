f6234b6e6498ce6097ebe19459a0e248587d27da
Robby Dhindsa <dhindsa.1996@gmail.com>
2022-12-26 18:02:09 -0800

    Added functions using return and console.log

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
+/*
 const sayHello = function (name) {
   console.log("Hello, " + name); 
 }
@@ -5,3 +6,16 @@ const sayHello = function (name) {
 sayHello("Caliban"); 
 sayHello("Miranda"); 
 sayHello("Ferdinand"); 
+*/
+
+const sayHelloToConsole = function(name) {
+  console.log("Hello, " + name); 
+}
+sayHelloToConsole("John"); 
+
+
+const returnSayHello = function(name) {
+  return "Hello, " + name; 
+}
+const greeting = returnSayHello("John"); 
+console.log(greeting); 
