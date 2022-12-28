4d251cb13ca26348e95f1fddf0aecd90d8908752
Robby Dhindsa <dhindsa.1996@gmail.com>
2022-12-27 20:31:52 -0800

    Added a while loop to show the same packing list

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,20 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+// FOR LOOP: 
+
+/*
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 } 
+*/ 
+
+
+// WHILE LOOP: 
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
